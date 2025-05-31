d2615a969f3c9817f813ecac693d1e9f0f5c1235
ALIGHASSOUN <alighassoun9@gmail.com>
2025-05-31 16:22:42 +0300

    First commit

---
+++ backend/models/user-mode.js
@@ -0,0 +1,33 @@
+// backend/models/User.js
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: ["admin", "branch"],
+      required: true,
+    },
+    branchName: {
+      type: String,
+      required: function () {
+        return this.role === "branch";
+      },
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const User = mongoose.model("User", userSchema);
---
+++ backend/server.js
@@ -0,0 +1,22 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cors from "cors";
+import { connectDB } from "./lib/db.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+app.listen(5000, () => {
+  console.log("Server is runing on http://localhost:" + PORT);
+  connectDB();
+});
